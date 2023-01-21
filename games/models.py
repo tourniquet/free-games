d22942756cd85139d53768b9330514ff25e24c8f
tourniquet <admyn3d@gmail.com>
2023-01-21 23:47:26 +0000

    created Genre and Game models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+  title = models.CharField(max_length=255, unique=True)
+
+  class Meta:
+    verbose_name_plural = 'genres'
+
+  def __str__(self):
+    return self.name
+
+
+class Game(models.Model):
+  title = models.CharField(max_length=255)
+  urls = models.CharField(max_length=255)
+  genres = models.ManyToManyField(Genre)
+  created = models.DateTimeField(auto_now_add=True)
+  expires = models.DateTimeField()
+  image = models.ImageField(upload_to='images', null=True, blank=True)
+
+  class Meta:
+    verbose_name_plural = 'games'
+
+  def __str__(self):
+    return self.name
