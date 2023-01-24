8c5daf7ebc1c3d5fe7ac7a4cabd632cfaeb9ab39
tourniquet <admyn3d@gmail.com>
2023-01-24 21:02:13 +0000

    settings for displaying images

--- freegames/settings.py
+++ freegames/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
