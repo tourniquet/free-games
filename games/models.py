c33dbf30cbb6f993c8c5e563af601438ad662d5d
tourniquet <admyn3d@gmail.com>
2023-01-24 23:56:59 +0000

    added Platform model

--- games/models.py
+++ games/models.py
@@ -11,10 +11,21 @@ class Genre(models.Model):
     return self.title
 
 
+class Platform(models.Model):
+  title = models.CharField(max_length=100, unique=True)
+
+  class Meta:
+    verbose_name_plural = 'platforms'
+
+  def __str__(self):
+    return self.title
+
+
 class Game(models.Model):
   title = models.CharField(max_length=255)
-  urls = models.CharField(max_length=255)
+  url = models.CharField(max_length=255)
   genres = models.ManyToManyField(Genre)
+  platform = models.ManyToManyField(Platform)
   created = models.DateTimeField(auto_now_add=True)
   expires = models.DateTimeField()
   image = models.ImageField(upload_to='images', null=True, blank=True)
