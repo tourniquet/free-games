beea2bdf69674c13d0b7d138534fe5895c9a4e1f
tourniquet <admyn3d@gmail.com>
2023-01-24 23:57:37 +0000

    basic views for requesting all games, by platform and by genre

--- games/views.py
+++ games/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Game, Genre, Platform
+
+
+def index(request):
+  games = Game.objects.all()
+  genres = Genre.objects.all()
+  context = {'games': games, "genres": genres}
+
+  return render(request, 'games/index.html', context)
+
+
+def by_genre(request, genre):
+  games = Game.objects.filter(genres__title=genre)
+  context = {"games": games}
+
+  return render(request, 'games/index.html', context)
+
+
+def by_platform(request, platform):
+  games = Game.objects.filter(platform__title=platform)
+  context = {'games': games}
+
+  return render(request, 'games/index.html', context)
